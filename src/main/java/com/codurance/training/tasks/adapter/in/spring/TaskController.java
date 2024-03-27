1a90944ad3d169c7cad4e331a8007522de048236
Arno <oejk97614@gmail.com>
2024-03-27 17:10:38 +0800

    Added TaskController to accept http for adding task and setting done.

---
+++ src/main/java/com/codurance/training/tasks/adapter/in/spring/TaskController.java
@@ -0,0 +1,64 @@
+package com.codurance.training.tasks.adapter.in.spring;
+
+import com.codurance.training.tasks.usecase.port.in.addtask.AddTaskInput;
+import com.codurance.training.tasks.usecase.port.in.addtask.AddTaskUseCase;
+import com.codurance.training.tasks.usecase.port.in.setdone.SetDoneInput;
+import com.codurance.training.tasks.usecase.port.in.setdone.SetDoneUseCase;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tw.teddysoft.ezddd.core.usecase.ExitCode;
+import tw.teddysoft.ezddd.cqrs.usecase.CqrsOutput;
+
+import static com.codurance.training.tasks.io.std.CheckListApp.CHECK_LIST_ID;
+
+@RestController
+public class TaskController {
+    private final AddTaskUseCase addTaskUseCase;
+    private final SetDoneUseCase setDoneUseCase;
+
+    @Autowired
+    public TaskController(AddTaskUseCase addTaskUseCase, SetDoneUseCase setDoneUseCase) {
+        this.addTaskUseCase = addTaskUseCase;
+        this.setDoneUseCase = setDoneUseCase;
+    }
+
+    @PostMapping(path = "/checkList/{checkListId}/project/{projectName}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<CqrsOutput> addTask(@PathVariable String checkListId,
+                                              @PathVariable String projectName,
+                                              @RequestBody String taskInfo) {
+
+        JSONObject taskJSON = new JSONObject(taskInfo);
+        String description = taskJSON.getString("description");
+
+        AddTaskInput addTaskInput = new AddTaskInput();
+        addTaskInput.setCheckListId(checkListId);
+        addTaskInput.setProjectName(projectName);
+        addTaskInput.setTaskDescription(description);
+        var output = addTaskUseCase.execute(addTaskInput);
+        if (output.getExitCode().equals(ExitCode.FAILURE)) {
+            return new ResponseEntity<>(output, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(output, HttpStatus.OK);
+    }
+
+    @PutMapping("/checkList/{checkListId}/project/{projectName}/{id}")
+    public ResponseEntity<CqrsOutput> setDone(@PathVariable String checkListId,
+                                              @PathVariable String id,
+                                              @PathVariable String projectName,
+                                              @RequestBody String taskInfo) {
+        JSONObject taskJSON = new JSONObject(taskInfo);
+        boolean flag = taskJSON.optBoolean("isDone");
+        SetDoneInput setTrueInput = new SetDoneInput();
+        setTrueInput.setCheckListId(checkListId);
+        setTrueInput.setTaskId(id);
+        setTrueInput.setDone(flag);
+        var output = setDoneUseCase.execute(setTrueInput);
+        if (output.getExitCode().equals(ExitCode.FAILURE)) {
+            return new ResponseEntity<>(output, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(output, HttpStatus.OK);
+    }
+}
